61fa19f1a2093ca1105d396c206f4a377c5b5028
Emmanuel Beulah <emmajessy2006@gmail.com>
2024-11-12 21:10:08 +0000

    Creating a boiler plate or future use wWeb scraping projects

---
+++ main.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+service = Service(executable_path="chromedriver.exe")
+driver = webdriver.Chrome(service=service)
+
+# ------------ Input link to automate in path --------------- #
+path = ""
+
+driver.get(path)

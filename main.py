38aa35fb6032f5c4c47f793e8d0e4797e51c2d20
Emmanuel Beulah <emmajessy2006@gmail.com>
2024-11-13 08:53:07 +0000

    Included Imports for handling Exceptions

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.common import (NoSuchElementException, NoSuchAttributeException, TimeoutException,
+                             ElementNotInteractableException)
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+# Select what browser is used
 service = Service(executable_path="chromedriver.exe")
+
+# Set driver options to determine behaviour
+
+#Initiate driver
 driver = webdriver.Chrome(service=service)
 
 # ------------ Input link to automate in path --------------- #
